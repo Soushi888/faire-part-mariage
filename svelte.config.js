7f02ead1c2c57eb03c4c16dd5b24e32df9ec55ae
soushi888 <sacha.pignot@protonmail.com>
2022-02-05 05:55:13 -0500

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,23 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import * as path from "path";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter(),
-
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
-	}
+  kit: {
+    adapter: adapter(),
+    vite: {
+      resolve: {
+        alias: {
+          // $assets: path.resolve("./static/assets")
+        }
+      }
+    }
+  }
 };
 
 export default config;
