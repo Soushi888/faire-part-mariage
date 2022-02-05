62f080384f65ad464051589619091132b0179d7f
soushi888 <sacha.pignot@protonmail.com>
2022-02-05 03:38:47 -0500

    reinit with svelte kit

---
+++ src/routes/index.svelte
@@ -0,0 +1,18 @@
+<!-- JS -->
+<script>
+
+</script>
+
+<!-- HTML -->
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<main>
+	<h1>Hello World !</h1>
+</main>
+
+<!-- CSS -->
+<style>
+
+</style>
